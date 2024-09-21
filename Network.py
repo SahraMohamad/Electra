e4d92cbdc1e2c51163256e7079a4b115e7bda310
Thomas <56136360+thom-bahm@users.noreply.github.com>
2024-09-21 18:45:57 -0400

    Update Network.py

--- Network.py
+++ Network.py
@@ -4,15 +4,20 @@ import openai
 import os
 
 class Network:
-    def __init__(self, task : str, identities : list, num_filler_agents : int):
+    def __init__(self, task : str):
         self.shared_task = task
-        self.identities = identities
+        self.identities = self._create_identities()
         self.shared_context = []
         self.client = openai.OpenAI(
             base_url='https://api.cerebras.ai/v1',
             api_key=os.environ.get("THOMAS_API_KEY")
         )
         self.agents = self._init_agents()
+        
+    def _create_identities():
+        identities = []
+        
+        return identities
         
     def _init_agents(self):
         agents = [Agent(task=self.shared_task, identity=identity, client=self.client) for identity in self.identities]
@@ -25,12 +30,15 @@ class Network:
                 for i, agent in enumerate(self.agents):
                     agent_response = agent.talk()
                     self.shared_context.append({"role": "assistant", "content": agent_response})
-                    print(f"Agent {i+1}: {agent_response}")
+                    print(f"agent {i+1}: {agent_response}")
             elif chat_type == "random":
                 for i in range(len(self.agents)):
                     agent = random.choice(self.agents)
                     agent_response = agent.talk()
-                    self.shared_context.append({"role": "npc", "content": agent_response})
-                    print(f"Agent {i+1}: {agent_response}")
+                    self.shared_context.append({"role": "agent", "content": agent_response})
+                    print(f"agent {i+1}: {agent_response}")
             round_count += 1
-        return self.shared_context    
+        return self.shared_context
+    
+    def simulate(self):
+        self.group_chat()
