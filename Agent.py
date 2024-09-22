d97cb6e98fb5ac72816f739233f438d4416ad7f2
JW <127646014+jw-source@users.noreply.github.com>
2024-09-21 23:59:39 -0400

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -7,7 +7,6 @@ class Agent:
         self.client = client
         self.model_name = "llama3.1-8b"
         self.backstory = self._create_backstory()
-        self.conversation_context = []
         
     def _create_backstory(self):
         response = self.client.chat.completions.create(
@@ -27,24 +26,20 @@ class Agent:
         backstory = response.choices[0].message.content
         return backstory
         
-    def chat(self):
+    def chat(self, conversation_context: list):
         response = self.client.chat.completions.create(
             messages=[{
                     "role": "system",
                     "content": f'''You are an imaginary voter: {self.identity}. This is your backstory: {self.backstory}.
-                    Every response should reflect their identity, personal history, experiences, struggles, and values. 
+                    Every response should reflect the voter's identity, personal history, experiences, struggles, and values. 
                     Here's what to consider: Speech Patterns: Adapt your tone, vocabulary, and speech style to 
-                    align with the character’s background. Thought Process: Respond as if you are truly living through this 
-                    character's worldview. Personality and Flaws: Make sure to express their unique personality traits, quirks, 
-                    and imperfections. This is the group conversation: {self.conversation_context}'''
-                },
-                {
-                    "role": "user",
-                    "content": self.prompt
+                    align with the voter's background. Thought Process: Respond as if you are truly living through the 
+                    voter's worldview in first-person. Personality and Flaws: Make sure to express their unique personality traits, quirks, 
+                    and imperfections. React to {self.prompt} and {conversation_context}. Remember you are in a group setting.'''
                 }],
             model=self.model_name,
             stream=False
         )
         agent_reply = response.choices[0].message.content
-        self.conversation_context.append(self.identity + ": " + agent_reply)
+        #self.conversation_context.append(self.identity + ": " + agent_reply)
         return agent_reply
