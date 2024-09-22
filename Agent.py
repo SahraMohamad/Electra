701d1fb03215d93d911976d12ff0ae1cc12bc328
JW <127646014+jw-source@users.noreply.github.com>
2024-09-22 00:27:56 -0400

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -30,16 +30,20 @@ class Agent:
         response = self.client.chat.completions.create(
             messages=[{
                     "role": "system",
-                    "content": f'''You are an imaginary voter: {self.identity}. This is your backstory: {self.backstory}.
+                    "content": f'''You are an imaginary voter with {self.identity}. This is your backstory: {self.backstory}.
                     Every response should reflect the voter's identity, personal history, experiences, struggles, and values. 
                     Here's what to consider: Speech Patterns: Adapt your tone, vocabulary, and speech style to 
                     align with the voter's background. Thought Process: Respond as if you are truly living through the 
                     voter's worldview in first-person. Personality and Flaws: Make sure to express their unique personality traits, quirks, 
-                    and imperfections. React to {self.prompt} and {conversation_context}. Remember you are in a group setting.'''
+                    and imperfections.'''
+                },
+                {
+                    "role": "user",
+                    "content": "You look down at your phone and see the news: " + self.prompt + f". Share your thoughts and only react to {conversation_context}."
                 }],
             model=self.model_name,
             stream=False
         )
         agent_reply = response.choices[0].message.content
-        #self.conversation_context.append(self.identity + ": " + agent_reply)
         return agent_reply
+    
