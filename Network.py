4bd09ac4100570d15aa618ec666b86be8b2c90a0
JW <127646014+jw-source@users.noreply.github.com>
2024-09-22 02:38:41 -0400

    Update Network.py

--- Network.py
+++ Network.py
@@ -29,15 +29,38 @@ class Network:
                 for i, agent in enumerate(self.agents):
                     agent_response = agent.chat(self.shared_context)
                     self.shared_context.append(agent.identity + ": " + agent_response)
+                    if len(self.shared_context) > 28000:
+                        self.shared_context = self.shared_context[-14000:]
                     print(f"\n{agent.identity}: {agent_response}")
             elif chat_type == "random":
                 for i in range(len(self.agents)):
                     agent = random.choice(self.agents)
                     agent_response = agent.chat(self.shared_context)
                     self.shared_context.append(agent.identity + ": " + agent_response)
+                    if len(self.shared_context) > 28000:
+                        self.shared_context = self.shared_context[-14000:]
                     print(f"\n{agent.identity}: {agent_response}")
             round_count += 1
         return self.shared_context
     
-    def simulate(self):
-        self.group_chat()
+    def simulate(self, chat_type, max_rounds):
+        pre_vote = [2]*len(self.agents)
+        post_vote = [2]*len(self.agents)
+        for i, agent in enumerate(self.agents):
+            pre_decision = agent.pre_predict()
+            pre_vote[i] = pre_decision
+        self.group_chat(chat_type, max_rounds)
+        for i, agent in enumerate(self.agents):
+            post_decision = agent.post_predict()
+            post_vote[i] = post_decision
+        print(f"Pre-vote: {pre_vote}")
+        print(f"Post-vote: {post_vote}")
+        change = (sum(post_vote) - sum(pre_vote))/len(pre_vote) * 100
+        change = round(change, 2)
+        if change < 0:
+            print(f"Harris' support increased by {-1*change}%")
+        elif change > 0:
+            print(f"Trump's support increased by {change}%")
+        else:
+            print("Voter support remained the same")
+        return pre_vote, post_vote
