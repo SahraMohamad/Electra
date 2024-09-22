45693586876362924efc54ecbec9cf3e31b3f858
Thomas <56136360+thom-bahm@users.noreply.github.com>
2024-09-21 20:01:48 -0400

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,23 +1,24 @@
 from openai import OpenAI
 
 class Agent:
-    def __init__(self, task : str, identity: str, client : OpenAI):
+    def __init__(self, prompt : str, identity: str, client : OpenAI):
         self.identity = identity
-        self.task = task
+        self.prompt = prompt
         self.system_prompt = self._create_system_prompt()
         self.model_name = "llama3.1-70b"
         self.client = client
         self.context = self._init_context()
         
-
     def _create_system_prompt(self):
         sys_prompt = {
             "role": "system",
             "content": f'''
-            This is your task: ###{self.task}###.
             This is your identity: ###{self.identity}###, 
+            And you have just been told this news: ###{self.prompt}###
             
-            You should behave as your identity suggests, and you should follow the instructions specified by the task closely, and not go off topic.
+            You should now begin to think about how this news will affect your voting disposition,
+            and you should openly discuss if hearing the news has or has not changed your opinion on who you will vote for
+            out of these two candidates: [Donald Trump, Kamala Harris]
             '''
         }
         
@@ -25,7 +26,7 @@ class Agent:
         
     def _init_context(self):
         return [self.system_prompt]
-                    
+        
     def talk(self):
         response = self.client.chat.completions.create(
             messages=self.context,
