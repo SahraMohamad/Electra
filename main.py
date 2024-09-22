56e0e0a5fbf671e612448b380c78cd5278db977d
JW <127646014+jw-source@users.noreply.github.com>
2024-09-21 22:58:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 from Network import Network
-from Agent import Agent
-from variables import identities
-def main():
-    task = '''
-    You are a members of a comittee which is trying to decide on a superhero name for a new marvel movie. 
-    You must find a solution by the end of your conversation.
-    You should output your final conclusion delimited by triple backticks.
-    '''
-    agent_network = Network(task=task, identities=identities, num_filler_agents=0)
-    agent_network.group_chat("round_robin", 5)
-    
-if __name__ == '__main__':
-    main()
+task = '''Breaking News! Donald Trump will launch his own cryptocurrency.'''
+agent_network = Network(task, "Texas", 15)
+agent_network.group_chat("round_robin", 1)
