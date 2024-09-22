1ab17dfff0a7f5fff116d95c60015f7b5d22a54d
JW <127646014+jw-source@users.noreply.github.com>
2024-09-21 22:53:38 -0400

    Update Network.py

--- Network.py
+++ Network.py
@@ -1,26 +1,25 @@
 from Agent import Agent
 import random 
 import openai
-import os
 from generate_identity import generate_identities
 
 class Network:
-    def __init__(self, task : str):
-        self.shared_task = task
-        self.identities = self._create_identities()
-        self.shared_context = []
+    def __init__(self, prompt : str, state: str, num_agents:int):
+        self.prompt = prompt
+        self.identities = self._create_identities(state, num_agents)
         self.client = openai.OpenAI(
             base_url='https://api.cerebras.ai/v1',
-            api_key=os.environ.get("THOMAS_API_KEY")
+            api_key=""
         )
+        self.shared_context = []
         self.agents = self._init_agents()
         
-    def _create_identities(state:str, num_voters:int):
-        identities = generate_identities(state, num_voters)
+    def _create_identities(self, state:str, num_agents:int):
+        identities = generate_identities(state, num_agents)
         return identities
         
     def _init_agents(self):
-        agents = [Agent(task=self.shared_task, identity=identity, client=self.client) for identity in self.identities]
+        agents = [Agent(prompt=self.prompt, identity=identity, client=self.client) for identity in self.identities]
         return agents
     
     def group_chat(self, chat_type, max_rounds):
@@ -28,15 +27,15 @@ class Network:
         while round_count < max_rounds: 
             if chat_type == "round_robin":
                 for i, agent in enumerate(self.agents):
-                    agent_response = agent.talk()
-                    self.shared_context.append({"role": "assistant", "content": agent_response})
-                    print(f"agent {i+1}: {agent_response}")
+                    agent_response = agent.chat()
+                    self.shared_context.append({"role": "agent", "content": agent_response})
+                    print(f"\n{agent.identity}: {agent_response}")
             elif chat_type == "random":
                 for i in range(len(self.agents)):
                     agent = random.choice(self.agents)
-                    agent_response = agent.talk()
+                    agent_response = agent.chat()
                     self.shared_context.append({"role": "agent", "content": agent_response})
-                    print(f"agent {i+1}: {agent_response}")
+                    print(f"\n{agent.identity}: {agent_response}")
             round_count += 1
         return self.shared_context
     
